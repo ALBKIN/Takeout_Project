79069c7b96e4bd8f5f37435815225c0206201ff1
ALBKIN <kinkel.albert@gmail.com>
2023-09-01 15:46:39 +0200

    Initial testing of TAKE-OUT application

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String customerName = input.nextLine();
 
+        int money = 0; // Initialize money
+
+        while (true) { // Loop until a valid integer is entered
+            System.out.println("Enter the amount of money you have: ");
+            try {
+                money = input.nextInt();
+                break; // Exit loop if money is succesfully read;
+            } catch (InputMismatchException e) {
+                System.out.println("Please provide valid money amount.");
+                input.nextLine(); // Clear the invalid input
+            }
+        }
+
+        Customer customer = new Customer(customerName, money);
+        TakeOutSimulator takeOutSimulator = new TakeOutSimulator(customer, new FoodMenu(),input);
+
+        // Start the simulator using the instance of TakeOutSimulator
+        takeOutSimulator.startTakeOutSimulator();
     }
 }
